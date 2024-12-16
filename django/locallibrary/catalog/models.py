214957d9fa65af3246b403fa54915322d8092001
UNCROOS <any492266@gmail.com>
2024-12-16 19:03:48 +0900

    모델 설정

--- django/locallibrary/catalog/models.py
+++ django/locallibrary/catalog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+
+    def __str__(self):
+        return self.name
