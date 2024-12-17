be37ac13aa4a232faa62f4d891541599b22bbec7
UNCROOS <any492266@gmail.com>
2024-12-17 23:12:01 +0900

    2일차 성공

--- django/locallibrary/catalog/views.py
+++ django/locallibrary/catalog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Book, Author, BookInstance, Genre
+
+def index(request):
+    num_books = Book.objects.all().count() # 책 오브젝트를 모두 가져오고 갯수를 카운트
+    num_instances = BookInstance.objects.all().count() # 책복사본 오브젝트를 다 가져오고 갯수를 카운트
+    
+    # 대출 가능한 책의 갯수를 카운트.
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # 작가를 모두 가져오고 갯수를 카운트
+    num_authors = Author.objects.count()
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # index.html에 변수를 render한다.
+    return render(request, 'index.html', context=context)
+
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
