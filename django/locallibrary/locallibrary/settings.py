7aaf45e8fff7784495569375d9a59100af169d5a
UNCROOS <any492266@gmail.com>
2024-12-18 20:13:06 +0900

    3일차

--- django/locallibrary/locallibrary/settings.py
+++ django/locallibrary/locallibrary/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 
 # Quick-start development settings - unsuitable for production
